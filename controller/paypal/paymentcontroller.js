e5e42b06c41b02d1f2f9a58ac29eb7238813b08a
Vipul Goyal <134921215+VipulGoyal95@users.noreply.github.com>
2025-07-18 18:38:05 +0530

    changed acces-control-allow-origin url

--- controller/paypal/paymentcontroller.js
+++ controller/paypal/paymentcontroller.js
@@ -8,7 +8,7 @@ paypal.configure({
 
 
 const createPayment = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://saenitkurukshetra.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.saenitkurukshetra.com');
     const {amount}=req.body;
     // console.log(amount);
     let data
